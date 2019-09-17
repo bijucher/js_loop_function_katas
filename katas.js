359ac5641d8746c27e46111da24e2b87932aa145
bijucher <53838347+bijucher@users.noreply.github.com>
2019-09-17 17:07:18 -0400

    site

--- katas.js
+++ katas.js
@@ -1,61 +1,106 @@
-(function(){
-    
-    
+(function () {
+
+
     function oneThroughTwenty() {
         const numbers = []
 
         // Your code goes below
-
+        for (let i = 1; i <= 20; i++) {
+            numbers.push(i)
+        }
         return numbers;
     }
+
+    console.log(oneThroughTwenty());
 
     function evensToTwenty() {
         const numbers = []
 
         // Your code goes below
+        for (let i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                numbers.push(i)
 
+            }
+        }
         return numbers;
     }
+
+    console.log(evensToTwenty())
+
 
     function oddsToTwenty() {
         const numbers = []
 
         // Your code goes below
+        for (let i = 0; i <= 20; i++) {
+            if (i % 2 == 1) {
+                numbers.push(i)
+            }
+        }
 
         return numbers;
     }
+    console.log("odds")
+    console.log(oddsToTwenty())
+
+
 
     function multiplesOfFive() {
         const numbers = []
 
         // Your code goes below
+        for (let i = 5; i <=100; i +=5) {
+            numbers.push(i)
+
+        
+        }
 
         return numbers;
     }
+
+    console.log(multiplesOfFive())
 
     function squareNumbers() {
         const numbers = []
 
         // Your code goes below
+        for (let i = 1; i <=100; i += 1) {
+            if(Math.sqrt(i) % 1 === 0){
+                numbers.push(i)
+            }
+            
+
+        
+        }
 
         return numbers;
     }
+
+    console.log(squareNumbers())
 
     function countingBackwards() {
         const numbers = []
 
         // Your code goes below
+        for (let i = 20; i >= 1; i--) {
+            numbers.push(i)
+        }
 
         return numbers;
     }
+    console.log(countingBackwards())
 
     function evenNumbersBackwards() {
         const numbers = []
 
         // Your code goes below
 
+        
+
         return numbers;
     }
+    console.log(evenNumbersBackwards())
 
     function oddNumbersBackwards() {
         const numbers = []
@@ -64,6 +109,7 @@
 
         return numbers;
     }
+    console.log(oddNumbersBackwards())
 
     function multiplesOfFiveBackwards() {
         const numbers = []
@@ -72,6 +118,7 @@
 
         return numbers;
     }
+    console.log(multiplesOfFiveBackwards())
 
     function squareNumbersBackwards() {
         const numbers = []
@@ -80,6 +127,6 @@
 
         return numbers;
     }
-    
-    
+    console.log(squareNumbersBackwards())
+
 })();
